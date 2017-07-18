4f2a0d519831262c6910670269d48873955bf1d3
UNUSA-03 <UNUSA-03>
2017-07-18 12:35:40 +0700

    install lib

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from fbmq import Page
+
+page = fbmq.Page(PAGE_ACCESS_TOKEN)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  page.handle_webhook(request.get_data(as_text=True))
+  return "ok"
+
+@page.handle_message
+def message_handler(event):
+  """:type event: fbmq.Event"""
+  sender_id = event.sender_id
+  message = event.message_text
+
+  page.send(sender_id, "thank you! your message is '%s'" % message)
+
+@page.after_send
+def after_send(payload, response):
+  """:type payload: fbmq.Payload"""
+  print("complete")
