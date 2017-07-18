6ce13b83381869f283ec40b6c3b1fcb069e743ea
UNUSA-03 <UNUSA-03>
2017-07-18 13:47:38 +0700

    'install'

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug=True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
